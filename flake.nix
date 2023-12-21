5294e94c151f0b26ef8916eaefaf76301870f857
Aurelia <aurelia@acuteaura.net>
2023-12-21 13:05:14 +0100

    fix flake exports oops

--- flake.nix
+++ flake.nix
@@ -8,9 +8,6 @@
   outputs = { self, nixpkgs, nixpkgs-unstable }: {
     nixosModules = {
       tranquility = ./systems/tranquility;
-      desktop = ./desktop.nix;
-      codium = ./codium.nix;
-      base = ./base.nix;
     };
     packages.x86_64-linux = {
       apisix-ingress-controller = nixpkgs.callPackage ./packages/apisix-ingress-controller.nix { };
