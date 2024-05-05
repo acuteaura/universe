da8ad83ee1a49b6fbd1fdf83519c0d6f6caad334
Aurelia <git@acuteaura.net>
2024-05-05 18:59:37 +0200

    remove graphical from default HM

--- homes/framework/default.nix
+++ homes/framework/default.nix
@@ -7,8 +7,6 @@
     ../_modules/hwtools.nix
     ../_modules/kube.nix
     ../_modules/security.nix
-
-    ../_modules/graphical.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
