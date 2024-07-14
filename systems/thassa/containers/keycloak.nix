8380017a2d683fc7ee4307c1ce87c310b8b7d8db
Aurelia <git@acuteaura.net>
2024-07-14 16:24:49 +0200

    whee

--- systems/thassa/containers/keycloak.nix
+++ systems/thassa/containers/keycloak.nix
@@ -28,4 +28,34 @@
       ];
     };
   };
+
+  virtualisation.quadlet.containers.keycloak = {
+    containerConfig = {
+      name = "keycloak";
+      hostname = "keycloak";
+      image = "quay.io/aurelias/keycloak:18b8bfb8b082eaff61b16f513b5ae6b1beab7eda@sha256:53c64ac1445d1da0c567f73b37a72f1da183882227a3cc6d3e65eb0e7bd64fab ";
+      volumes = [
+        "/data/keycloak/postgres:/var/lib/postgresql/data:U"
+      ];
+      environments = {
+        "KC_DB" = "postgres";
+        "KC_DB_URL_HOST" = "keycloak_postgres";
+        "KC_DB_URL_DATABASE" = "postgres";
+        "KC_DB_USERNAME" = "postgres";
+        "KC_HOSTNAME" = "id.nullvoid.space";
+        "KEYCLOAK_ADMIN" = "root";
+      };
+      environmentFiles = [
+        "/etc/keycloak.env"
+      ];
+      networks = ["keycloak.network"];
+    };
+    unitConfig = {
+      After = ["network.target"];
+      Wants = ["network.target"];
+      RequiresMountsFor = [
+        "/data/keycloak"
+      ];
+    };
+  };
 }
