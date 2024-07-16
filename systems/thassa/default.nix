18633f7c40b9d2f03019be0d566f256c161ae466
Aurelia <git@acuteaura.net>
2024-07-16 10:52:59 +0200

    nix firewall bonk

--- systems/thassa/default.nix
+++ systems/thassa/default.nix
@@ -25,6 +25,9 @@
 
   programs._1password.enable = true;
 
+  # nixos sucks tbh
+  networking.firewall.interfaces."podman+".allowedUDPPorts = [53 5353];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
