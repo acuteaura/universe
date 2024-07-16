18121de9a996bd411bb0efa1ce3c414e1c6cdfbf
Aurelia <git@acuteaura.net>
2024-07-16 11:04:15 +0200

    not justifying myself

--- systems/thassa/nginx.nix
+++ systems/thassa/nginx.nix
@@ -1,22 +1,21 @@
 { pkgs, ... }: {
   services.nginx = {
     enable = true;
-  };
 
-  recommendedGzipSettings = true;
-  recommendedOptimisation = true;
-  recommendedProxySettings = true;
-  recommendedTlsSettings = true;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
 
-  virtualHosts."id.nullvoid.space" = {
-    addSSL = true;
-    enableACME = true;
-    locations."/" = {
-      proxyPass = "http://127.0.0.1:8080";
-      proxyWebsockets = true;
-      extraConfig = ''
-        client_max_body_size 256M;
-      '';
+    virtualHosts."id.nullvoid.space" = {
+      addSSL = true;
+      enableACME = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8080";
+        proxyWebsockets = true;
+        extraConfig = ''
+          client_max_body_size 256M;
+        '';
+      };
     };
-  };
-}
+  }
