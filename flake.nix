3e8809be5e3c6582d9f2485333b794e781d40725
Aurelia <git@acuteaura.net>
2024-10-25 13:57:42 +0200

    chore: add unstable special arg to thassa

--- flake.nix
+++ flake.nix
@@ -62,6 +62,7 @@
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.aurelia = import ./homes/shell-linux;
+              home-manager.extraSpecialArgs = { inherit unstable; };
             }
           ];
         };
