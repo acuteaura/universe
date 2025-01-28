28b8a87057be8727fd1e66ccc6c9313c9367fb26
Aurelia <git@acuteaura.net>
2025-01-28 20:39:27 +0100

    update 2025-01-28 19:39:26+0000

--- systems/_modules/base.nix
+++ systems/_modules/base.nix
@@ -52,7 +52,5 @@
   nix.package = pkgs.lix;
 
   # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
+
 }
