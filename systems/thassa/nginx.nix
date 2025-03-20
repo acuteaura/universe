14df7aacffedfbf1237aac7e5db3ac5fd2377d8d
Aurelia <git@acuteaura.net>
2025-03-20 21:56:51 +0100

    update 2025-03-20 20:56:50+0000

--- systems/thassa/nginx.nix
+++ systems/thassa/nginx.nix
@@ -91,6 +91,10 @@
           client_max_body_size 256M;
         '';
       };
+      locations."/resources" = {
+        proxyPass = "http://127.0.0.1:8080";
+        proxyWebsockets = true;
+      };
       locations."/realms/nvs/metrics" = {
         extraConfig = ''
           deny all;
