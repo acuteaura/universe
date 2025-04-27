ac91eb543661f181e662ff5c9118c0b0d996a8e6
Aurelia <git@acuteaura.net>
2025-04-27 13:23:45 +0200

    update 2025-04-27 11:23:45+0000

--- systems/fool/hardware.nix
+++ systems/fool/hardware.nix
@@ -39,7 +39,7 @@
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/6C4EE0214EDFE23E";
+    device = "/dev/disk/by-uuid/9910-D057";
     fsType = "vfat";
     options = ["fmask=0022" "dmask=0022"];
   };
