017d7716f5eb3a7180f8c8aeaa8b23f22f513eca
Aurelia <git@acuteaura.net>
2025-04-27 12:54:44 +0200

    update 2025-04-27 10:54:44+0000

--- systems/fool/default.nix
+++ systems/fool/default.nix
@@ -42,20 +42,18 @@
 
   time.timeZone = "Europe/Berlin";
 
-  users.groups.aurelia = {
+    users.groups.aurelia = {
     name = "aurelia";
     gid = 1000;
   };
-
   users.users.aurelia = {
     isNormalUser = true;
     group = "aurelia";
     extraGroups = ["wheel" "docker"];
-    packages = with pkgs; [];
     openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJmjGIsSO9jE85xNPzzp0AWfOSXVL4qQ3cuXeKCvxe+q"];
     shell = pkgs.fish;
   };
-
+  nix.settings.trusted-users = ["aurelia"];
   programs._1password-gui.polkitPolicyOwners = ["aurelia"];
   environment.etc."1password/custom_allowed_browsers" = {
     text = ''
