f84b4800ef053cdfbff040ec2e55e11c2a3d4869
Aurelia <git@acuteaura.net>
2025-04-27 12:55:32 +0200

    update 2025-04-27 10:55:32+0000

--- systems/fool/default.nix
+++ systems/fool/default.nix
@@ -87,8 +87,6 @@
 
   programs.nix-ld.enable = true;
 
-  nix.settings.trusted-users = "aurelia";
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
