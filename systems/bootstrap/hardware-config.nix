57c3d1d64345abcacc3dd1090e876ead6984db22
Aurelia <git@acuteaura.net>
2025-09-22 11:41:19 +0200

    update from justice@2025-09-22 09:41:19+0000

--- systems/bootstrap/hardware-config.nix
+++ systems/bootstrap/hardware-config.nix
@@ -1,18 +1,13 @@
 {modulesPath, ...}: {
   imports = [(modulesPath + "/profiles/qemu-guest.nix")];
-  boot.loader.grub = {
-    efiSupport = true;
-    efiInstallAsRemovable = true;
-    device = "nodev";
-  };
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/7AE2-203E";
-    fsType = "vfat";
-  };
-  boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront"];
-  boot.initrd.kernelModules = ["nvme"];
-  fileSystems."/" = {
-    device = "/dev/sda1";
-    fsType = "ext4";
-  };
+  boot.loader.systemd-boot.enable = true;
+
+  boot.kernelParams = [ "console=tty" ];
+  boot.initrd.kernelModules = [ "virtio_gpu" ];
+
+  fileSystems."/boot" = { device = "/dev/disk/by-uuid/7763-8830"; fsType = "vfat"; };
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
+  boot.initrd.kernelModules = [ "nvme" ];
+  fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
+
 }
