73bb1cb313c721d6e336e042a330b73013ec88fb
Aurelia <git@acuteaura.net>
2025-09-22 11:42:48 +0200

    update from justice@2025-09-22 09:42:48+0000

--- systems/bootstrap/hardware-config.nix
+++ systems/bootstrap/hardware-config.nix
@@ -3,11 +3,11 @@
   boot.loader.systemd-boot.enable = true;
 
   boot.kernelParams = [ "console=tty" ];
-  boot.initrd.kernelModules = [ "virtio_gpu" ];
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
+  boot.initrd.kernelModules = [ "nvme" "virtio_gpu" ];
 
   fileSystems."/boot" = { device = "/dev/disk/by-uuid/7763-8830"; fsType = "vfat"; };
-  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
-  boot.initrd.kernelModules = [ "nvme" ];
+
   fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
 
 }
