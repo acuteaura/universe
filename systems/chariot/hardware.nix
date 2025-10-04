3d61214e6dc07f6ef0d3b2ed7128244bd821276e
Aurelia <git@acuteaura.net>
2025-10-04 10:27:46 +0200

    update from fool@2025-10-04 08:27:46+0000

--- systems/chariot/hardware.nix
+++ systems/chariot/hardware.nix
@@ -10,8 +10,6 @@
   # https://github.com/NixOS/nixos-hardware/blob/106d3fec43bcea19cb2e061ca02531d54b542ce3/framework/13-inch/common/default.nix
   hardware.sensor.iio.enable = true;
 
-  boot.zfs.devNodes = "/dev/disk/by-label/";
-
   fileSystems."/boot" = {
     device = "/dev/disk/by-label/LBOOT";
     fsType = "vfat";
