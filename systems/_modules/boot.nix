5feb4b26c3ddb7285d2a06b70431fe6dbb541579
Aurelia <git@acuteaura.net>
2025-10-19 13:37:22 +0200

    update from fool@2025-10-19 11:37:22+0000

--- systems/_modules/boot.nix
+++ systems/_modules/boot.nix
@@ -9,23 +9,22 @@
 
   config = let
     cfg = config.universe.secureboot.enable;
-  in if cfg.enable then {
-    boot.lanzaboote = {
-      enable = false;
-      pkiBundle = "/var/lib/sbctl";
-    };
-  } else {
-    # BOOT
-    #########################################
-    boot.loader = {
-      systemd-boot = {
-        enable = true;
-        configurationLimit = 10;
-        consoleMode = "max";
-        rebootForBitlocker = true;
-        memtest86.enable = true;
+  in {
+    boot = {
+      lanzaboote = {
+        enable = if cfg.enable then true else false;
+        lanzaboote.pkiBundle = "/var/lib/sbctl";
       };
-      efi.canTouchEfiVariables = true;
+      loader = {
+        systemd-boot = {
+          enable = if cfg.enable then false else true;
+          configurationLimit = 10;
+          consoleMode = "max";
+          rebootForBitlocker = true;
+          memtest86.enable = true;
+        };
+        efi.canTouchEfiVariables = true;
+      };
     };
   };
 }
